259c3d84833e791a55743fdd72d7861950dd9ca4
judygabeskiria <gabeskiriai@student.mville.edu>
2017-08-05 11:28:50 +0400

    first commit

---
+++ cashier.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = 7;
+        int[] w = new int[n];
+        for(int i=0; i < n; i++){
+            a[i] = in.nextInt();
+        }
+        int n=in.nextInt();
+        int j=in.nextInt();
+        
+    }
+}
