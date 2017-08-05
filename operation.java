e260a25fa9244d6782c89611b998f25b27ec4a09
judygabeskiria <gabeskiriai@student.mville.edu>
2017-08-05 11:30:56 +0400

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] w = new int[n];
+        for(int i=0; i < n; i++){
+            w[i] = in.nextInt();
+        }
+        int j=in.nextInt();
+    }
+}
